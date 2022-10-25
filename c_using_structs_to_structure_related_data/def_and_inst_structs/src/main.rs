6c5cefee2f05802340777f696fbab049ac654487
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-10-25 05:46:04 +0200

    Restructure def_and_inst_structs crate into modules according to chapt 7

--- c_using_structs_to_structure_related_data/def_and_inst_structs/src/main.rs
+++ c_using_structs_to_structure_related_data/def_and_inst_structs/src/main.rs
@@ -1,79 +1,103 @@
 // Create user struct with 4 fields, username, email, active, sign_in_count
-struct User {
-    email: String,
-    username: String,
-    active: bool,
-    sign_in_count: u64,
-}
 
 fn main() {
-    // create unmutable user
-    let user1 = User {
-        email: String::from("myemail@email.com"),
-        username: String::from("My Name"),
-        active: true,
-        sign_in_count: 1,
-    };
-    print_user(&user1);
-
-    let email2 = String::from("bob@gmail");
-    let username2 = String::from("bob");
-    let user2 = create_user(email2, username2);
-    print_user(&user2);
-    print_user(&user2);
-
-    // Mutability
-    let mut user3 = User {
-        email: String::from("john@email.com"),
-        username: String::from("john"),
-        active: true,
-        sign_in_count: 1,
-    };
-    print_user(&user3);
-    user3.email = String::from("john.j@gmail.com");
-    println!("User email has been updated");
-    print_user(&user3);
-
-    // ===== Struct update syntax - ====
-    //  user4 will get new values for email and username
-    // and then active and sign_in_count will use the user3 values
-    // note: in this case user 3 and user 4 can be used again
-    // because active and sign_in_count are 'copy'
-
-    let user4 = User {
-        email: String::from("sarah@email.com"),
-        username: String::from("sarah"),
-        ..user3
-    };
-    print_user(&user3);
-    print_user(&user4);
-    // note: in this case user 4 cannot be used after this as
-    // email and username are 'move' and not 'copy'
-
-    let user5 = User {
-        active: false,
-        sign_in_count: 2,
-        ..user4
-    };
-    print_user(&user5);
-    // print_user(&user4); <-- this line would error
+    user_examples::immutable_users();
+    user_examples::mutable_users();
+    user_examples::struct_update_syntax()
 }
 
-fn create_user(email: String, username: String) -> User {
-    User {
-        email, // note use of shorthand --> this is equivalent to email: email (like es6)
-        username,
-        active: true,
-        sign_in_count: 1,
+mod user_utils {
+    pub struct User {
+        pub email: String,
+        pub username: String,
+        pub active: bool,
+        pub sign_in_count: u64,
     }
-}
-
-fn print_user(user: &User) {
-    println!(
+    pub fn print_user(user: &User) {
+        println!(
         "\n\nEmail:\t\t{email}\nUsername:\t{username}\nActive:\t\t{active}\nSignin Count:\t{si_count}",
         email = user.email,
         username = user.username,
         active = user.active,
         si_count = user.sign_in_count
-    );
+        );
+    }
+
+    pub fn create_user(email: String, username: String) -> User {
+        User {
+            email, // note use of shorthand --> this is equivalent to email: email (like es6)
+            username,
+            active: true,
+            sign_in_count: 1,
+        }
+    }
+}
+
+mod user_examples {
+    use crate::user_utils::create_user;
+    use crate::user_utils::print_user;
+    use crate::user_utils::User;
+
+    pub fn immutable_users() {
+        // create unmutable user
+        let user1 = User {
+            email: String::from("myemail@email.com"),
+            username: String::from("My Name"),
+            active: true,
+            sign_in_count: 1,
+        };
+        print_user(&user1);
+
+        let email2 = String::from("bob@gmail");
+        let username2 = String::from("bob");
+        let user2 = create_user(email2, username2);
+        print_user(&user2);
+        print_user(&user2);
+    }
+
+    pub fn mutable_users() {
+        // Mutability
+        let mut user3 = User {
+            email: String::from("john@email.com"),
+            username: String::from("john"),
+            active: true,
+            sign_in_count: 1,
+        };
+        print_user(&user3);
+        user3.email = String::from("john.j@gmail.com");
+        println!("User email has been updated");
+        print_user(&user3);
+    }
+
+    pub fn struct_update_syntax() {
+        // ===== Struct update syntax - ====
+        //  user4 will get new values for email and username
+        // and then active and sign_in_count will use the user3 values
+        // note: in this case user 3 and user 4 can be used again
+        // because active and sign_in_count are 'copy'
+        let user4 = User {
+            email: String::from("john@email.com"),
+            username: String::from("john"),
+            active: true,
+            sign_in_count: 1,
+        };
+
+        let user5 = User {
+            email: String::from("sarah@email.com"),
+            username: String::from("sarah"),
+            ..user4
+        };
+        print_user(&user4);
+        print_user(&user5);
+        // note: in this case user 4 cannot be used after this as
+        // email and username are 'move' and not 'copy'
+
+        let user5 = User {
+            active: false,
+            sign_in_count: 2,
+            ..user4
+        };
+        print_user(&user5);
+        // print_user(&user4); <-- this line would error
+    }
 }
