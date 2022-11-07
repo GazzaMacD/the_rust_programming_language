68500f28e925520b704df01b9454a2fe8f3a9109
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-11-07 04:15:45 +0100

    Basic example of enum method

--- d_enums_and_pattern_matching/defining_an_enum/src/main.rs
+++ d_enums_and_pattern_matching/defining_an_enum/src/main.rs
@@ -2,7 +2,33 @@ enum MyIpAddr {
     V4(u8, u8, u8, u8),
     V6(String),
 }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+impl Coin {
+    fn value_in_cents(&self) -> u8 {
+        match self {
+            Self::Penny => 1,
+            Self::Nickel => 5,
+            Self::Dime => 10,
+            Self::Quarter => 25,
+        }
+    }
+}
+
 fn main() {
     let _home = MyIpAddr::V4(127, 0, 0, 1);
     let _loopback = MyIpAddr::V6(String::from("::1"));
+    let coin1 = Coin::Dime;
+    println!("Value of coin1 is {}", coin1.value_in_cents())
 }
