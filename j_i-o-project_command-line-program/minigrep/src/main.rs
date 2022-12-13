ab1059c7996475050420524ad9f16b32030b02be
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-12-13 11:35:11 +0100

    Change new to build and use Result for error handling rather than panic

--- j_i-o-project_command-line-program/minigrep/src/main.rs
+++ j_i-o-project_command-line-program/minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::env;
 use std::fs;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,16 +23,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!(
+            return Err(
                 "\nMore args needed. Try cargo run -- <search word> <path to file>\n
-                Example: cargo run -- Bob my_files/bob_email.txt\n
-            "
-            )
+                Example: cargo run -- Bob my_files/bob_email.txt\n ",
+            );
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
